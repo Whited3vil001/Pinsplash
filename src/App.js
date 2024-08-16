50c4a37461a133c7ec7724f4795aec07b956d286
Anand Kushwaha <ak9305059300.001@gmail.com>
2024-08-16 23:36:19 +0530

    updated the css for differebt devices 😮‍💨😁

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from "react";
 import Feed from "./components/Feed";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faComment, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faBell,
+  faComment,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <div className="logos">
-        <img className="logo" src="/favicon.png"></img>
-        <h1>Pinterest</h1>
-        <h2>
+        <h1>Pinsplash</h1>
+        <div className="bicon hidden">
           <a href="#" className="button">
             <FontAwesomeIcon icon={faBell} />
           </a>
-          <a href="#" className="button">
+          <a href="#" className="button hidden">
             <FontAwesomeIcon icon={faComment} />
           </a>
-          <a href="#" className="button">
+          <a href="#" className="button hidden">
             <FontAwesomeIcon icon={faUser} />
           </a>
-        </h2>
+        </div>
+        <div>
+          <a href="#" className="button show">
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+        </div>
       </div>
       <Feed />
     </div>
