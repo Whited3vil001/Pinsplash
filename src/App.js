c63a0162b1fa6e68b707910c242162e5a7b6f3c6
Anand Kushwaha <ak9305059300.001@gmail.com>
2024-08-16 11:28:08 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Feed from "./components/Feed";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Pinterest Clone</h1>
       </header>
       <Feed />
+      <Analytics />
     </div>
   );
 }
